ef43a84e9d846f50567ad915d1debd1ee86c8cff
DonSanoj <sanojaminda@gmail.com>
2024-02-28 14:46:03 +0530

    Create dashboard

---
+++ spend-smart/controlForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace spend_smart
+{
+    partial class controlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // controlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(96)))), ((int)(((byte)(96)))));
+            this.ClientSize = new System.Drawing.Size(1418, 968);
+            this.Name = "controlForm";
+            this.Text = "controlForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ spend-smart/controlForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spend_smart
+{
+    public partial class controlForm : Form
+    {
+        public controlForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
