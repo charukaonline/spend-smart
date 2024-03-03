c6d7fd4556826871786dd50a23477d852741e126
DonSanoj <snaojaminda@gmail.com>
2024-03-03 21:48:57 +0530

    Create menu bar

--- spend-smart/menuControls.cs
+++ spend-smart/menuControls.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,28 @@ namespace spend_smart
 {
     public partial class menuControls : Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
         public menuControls()
         {
             InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 18, 18));
+        }
+
+        private void menuControls_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
