da484f0a479e0c3fc7f3202baabbdddde62915c6
charukaonline <nuwanthakonline@gmail.com>
2024-03-04 09:33:22 +0530

    resources update

--- spend-smart/Properties/Resources.Designer.cs
+++ spend-smart/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace spend_smart.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap loading_image {
+            get {
+                object obj = ResourceManager.GetObject("loading_image", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap logo {
             get {
                 object obj = ResourceManager.GetObject("logo", resourceCulture);
