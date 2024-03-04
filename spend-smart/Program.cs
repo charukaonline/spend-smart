6109a0af631ee38a8ad7b37ec6ee0a938c64dc88
DonSanoj <sanojaminda@gmail.com>
2024-03-04 11:57:27 +0530

    create dashboard

--- spend-smart/Program.cs
+++ spend-smart/Program.cs
@@ -21,7 +21,7 @@ namespace spend_smart
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new menuControls());
         }
 
         [System.Runtime.InteropServices.DllImport("user32.dll")]
