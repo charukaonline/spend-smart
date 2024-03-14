08a8293e4e66a242c9e03d032f5811228754a603
mhnkarunarathna <hirushanuwanpriya17@gmail.com>
2024-03-14 15:44:27 +0530

    Create LoginPage

---
+++ spend-smart/loginForm.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace spend_smart
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.passwordTxtBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.userNameTxt = new Guna.UI2.WinForms.Guna2TextBox();
+            this.loginBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.usernameValidationLbl = new System.Windows.Forms.Label();
+            this.pinValidationLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2CirclePictureBox1
+            // 
+            this.guna2CirclePictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.guna2CirclePictureBox1.Image = global::spend_smart.Properties.Resources.logo;
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(33, 33);
+            this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox1.ShadowDecoration.Parent = this.guna2CirclePictureBox1;
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.guna2CirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2CirclePictureBox1.TabIndex = 0;
+            this.guna2CirclePictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(278, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome back!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(199, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(199, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "PIN";
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2TextBox1.BorderRadius = 10;
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(96)))), ((int)(((byte)(96)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.FocusedState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2TextBox1.ForeColor = System.Drawing.Color.White;
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Location = new System.Drawing.Point(464, 835);
+            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderText = "";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.ShadowDecoration.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Size = new System.Drawing.Size(845, 95);
+            this.guna2TextBox1.TabIndex = 2;
+            // 
+            // passwordTxtBox
+            // 
+            this.passwordTxtBox.BorderColor = System.Drawing.Color.Transparent;
+            this.passwordTxtBox.BorderRadius = 10;
+            this.passwordTxtBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.passwordTxtBox.DefaultText = "";
+            this.passwordTxtBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.passwordTxtBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.passwordTxtBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.passwordTxtBox.DisabledState.Parent = this.passwordTxtBox;
+            this.passwordTxtBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.passwordTxtBox.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(96)))), ((int)(((byte)(96)))));
+            this.passwordTxtBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.passwordTxtBox.FocusedState.Parent = this.passwordTxtBox;
+            this.passwordTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTxtBox.ForeColor = System.Drawing.Color.White;
+            this.passwordTxtBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.passwordTxtBox.HoverState.Parent = this.passwordTxtBox;
+            this.passwordTxtBox.Location = new System.Drawing.Point(204, 333);
+            this.passwordTxtBox.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.passwordTxtBox.Name = "passwordTxtBox";
+            this.passwordTxtBox.PasswordChar = '*';
+            this.passwordTxtBox.PlaceholderText = "";
+            this.passwordTxtBox.SelectedText = "";
+            this.passwordTxtBox.ShadowDecoration.Parent = this.passwordTxtBox;
+            this.passwordTxtBox.Size = new System.Drawing.Size(390, 40);
+            this.passwordTxtBox.TabIndex = 2;
+            this.passwordTxtBox.TextChanged += new System.EventHandler(this.passwordTxtBox_TextChanged);
+            this.passwordTxtBox.Enter += new System.EventHandler(this.passwordTxtBox_Enter);
+            // 
+            // userNameTxt
+            // 
+            this.userNameTxt.BorderColor = System.Drawing.Color.Transparent;
+            this.userNameTxt.BorderRadius = 10;
+            this.userNameTxt.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.userNameTxt.DefaultText = "";
+            this.userNameTxt.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.userNameTxt.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.userNameTxt.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.userNameTxt.DisabledState.Parent = this.userNameTxt;
+            this.userNameTxt.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.userNameTxt.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(96)))), ((int)(((byte)(96)))));
+            this.userNameTxt.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.userNameTxt.FocusedState.Parent = this.userNameTxt;
+            this.userNameTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNameTxt.ForeColor = System.Drawing.Color.White;
+            this.userNameTxt.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.userNameTxt.HoverState.Parent = this.userNameTxt;
+            this.userNameTxt.Location = new System.Drawing.Point(204, 207);
+            this.userNameTxt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.userNameTxt.Name = "userNameTxt";
+            this.userNameTxt.PasswordChar = '\0';
+            this.userNameTxt.PlaceholderText = "";
+            this.userNameTxt.SelectedText = "";
+            this.userNameTxt.ShadowDecoration.Parent = this.userNameTxt;
+            this.userNameTxt.Size = new System.Drawing.Size(390, 40);
+            this.userNameTxt.TabIndex = 2;
+            this.userNameTxt.TextChanged += new System.EventHandler(this.userNameTxt_TextChange);
+            this.userNameTxt.Enter += new System.EventHandler(this.userNameTxt_Enter);
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.BorderRadius = 10;
+            this.loginBtn.CheckedState.Parent = this.loginBtn;
+            this.loginBtn.CustomImages.Parent = this.loginBtn;
+            this.loginBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(212)))));
+            this.loginBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.ForeColor = System.Drawing.Color.White;
+            this.loginBtn.HoverState.Parent = this.loginBtn;
+            this.loginBtn.Location = new System.Drawing.Point(306, 434);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.ShadowDecoration.Parent = this.loginBtn;
+            this.loginBtn.Size = new System.Drawing.Size(180, 45);
+            this.loginBtn.TabIndex = 3;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(222, 495);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Don\'t have an Account? ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(212)))));
+            this.label5.Location = new System.Drawing.Point(440, 495);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(142, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Register here...";
+            // 
+            // usernameValidationLbl
+            // 
+            this.usernameValidationLbl.AutoSize = true;
+            this.usernameValidationLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameValidationLbl.ForeColor = System.Drawing.Color.Red;
+            this.usernameValidationLbl.Location = new System.Drawing.Point(200, 252);
+            this.usernameValidationLbl.Name = "usernameValidationLbl";
+            this.usernameValidationLbl.Size = new System.Drawing.Size(172, 20);
+            this.usernameValidationLbl.TabIndex = 5;
+            this.usernameValidationLbl.Text = "Enter valid user name";
+            // 
+            // pinValidationLbl
+            // 
+            this.pinValidationLbl.AutoSize = true;
+            this.pinValidationLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pinValidationLbl.ForeColor = System.Drawing.Color.Red;
+            this.pinValidationLbl.Location = new System.Drawing.Point(200, 380);
+            this.pinValidationLbl.Name = "pinValidationLbl";
+            this.pinValidationLbl.Size = new System.Drawing.Size(120, 20);
+            this.pinValidationLbl.TabIndex = 5;
+            this.pinValidationLbl.Text = "Enter valid PIN";
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(780, 561);
+            this.Controls.Add(this.pinValidationLbl);
+            this.Controls.Add(this.usernameValidationLbl);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.userNameTxt);
+            this.Controls.Add(this.passwordTxtBox);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2CirclePictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "loginForm";
+            this.Padding = new System.Windows.Forms.Padding(30);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "loginForm";
+            this.Load += new System.EventHandler(this.loginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2TextBox passwordTxtBox;
+        private Guna.UI2.WinForms.Guna2TextBox userNameTxt;
+        private Guna.UI2.WinForms.Guna2Button loginBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label usernameValidationLbl;
+        private System.Windows.Forms.Label pinValidationLbl;
+    }
+}
