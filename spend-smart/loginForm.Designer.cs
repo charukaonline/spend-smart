f7a7f5ddb66907759ecef1a2c541304190a9bfec
mhnkarunarathna <hirushanuwanpriya17@gmail.com>
2024-03-15 11:36:13 +0530

    Update Login Form

--- spend-smart/loginForm.Designer.cs
+++ spend-smart/loginForm.Designer.cs
@@ -40,7 +40,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.usernameValidationLbl = new System.Windows.Forms.Label();
             this.pinValidationLbl = new System.Windows.Forms.Label();
-            this.lblLockOutTimer = new System.Windows.Forms.Label();
+            this.labelLockoutTimer = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,7 +49,7 @@
             this.guna2CirclePictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.guna2CirclePictureBox1.Image = global::spend_smart.Properties.Resources.logo;
             this.guna2CirclePictureBox1.Location = new System.Drawing.Point(44, 41);
-            this.guna2CirclePictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.guna2CirclePictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
             this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.guna2CirclePictureBox1.ShadowDecoration.Parent = this.guna2CirclePictureBox1;
@@ -63,7 +63,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(371, 64);
+            this.label1.Location = new System.Drawing.Point(312, 68);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(273, 42);
@@ -75,7 +75,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(255, 222);
+            this.label2.Location = new System.Drawing.Point(189, 228);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(110, 25);
@@ -87,7 +87,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(255, 374);
+            this.label3.Location = new System.Drawing.Point(189, 378);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 25);
@@ -140,7 +140,7 @@
             this.passwordTxtBox.ForeColor = System.Drawing.Color.White;
             this.passwordTxtBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.passwordTxtBox.HoverState.Parent = this.passwordTxtBox;
-            this.passwordTxtBox.Location = new System.Drawing.Point(262, 414);
+            this.passwordTxtBox.Location = new System.Drawing.Point(194, 414);
             this.passwordTxtBox.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
             this.passwordTxtBox.Name = "passwordTxtBox";
             this.passwordTxtBox.PasswordChar = '*';
@@ -170,7 +170,7 @@
             this.userNameTxt.ForeColor = System.Drawing.Color.White;
             this.userNameTxt.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.userNameTxt.HoverState.Parent = this.userNameTxt;
-            this.userNameTxt.Location = new System.Drawing.Point(262, 259);
+            this.userNameTxt.Location = new System.Drawing.Point(194, 259);
             this.userNameTxt.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.userNameTxt.Name = "userNameTxt";
             this.userNameTxt.PasswordChar = '\0';
@@ -191,8 +191,8 @@
             this.loginBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.loginBtn.ForeColor = System.Drawing.Color.White;
             this.loginBtn.HoverState.Parent = this.loginBtn;
-            this.loginBtn.Location = new System.Drawing.Point(393, 525);
-            this.loginBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loginBtn.Location = new System.Drawing.Point(333, 513);
+            this.loginBtn.Margin = new System.Windows.Forms.Padding(4);
             this.loginBtn.Name = "loginBtn";
             this.loginBtn.ShadowDecoration.Parent = this.loginBtn;
             this.loginBtn.Size = new System.Drawing.Size(240, 55);
@@ -205,7 +205,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(304, 611);
+            this.label4.Location = new System.Drawing.Point(239, 611);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(225, 25);
@@ -215,9 +215,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(212)))));
-            this.label5.Location = new System.Drawing.Point(537, 611);
+            this.label5.Location = new System.Drawing.Point(472, 611);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(142, 25);
@@ -229,44 +230,43 @@
             this.usernameValidationLbl.AutoSize = true;
             this.usernameValidationLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.usernameValidationLbl.ForeColor = System.Drawing.Color.Red;
-            this.usernameValidationLbl.Location = new System.Drawing.Point(257, 314);
+            this.usernameValidationLbl.Location = new System.Drawing.Point(190, 314);
             this.usernameValidationLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.usernameValidationLbl.Name = "usernameValidationLbl";
-            this.usernameValidationLbl.Size = new System.Drawing.Size(172, 20);
+            this.usernameValidationLbl.Size = new System.Drawing.Size(178, 20);
             this.usernameValidationLbl.TabIndex = 5;
-            this.usernameValidationLbl.Text = "Enter valid user name";
+            this.usernameValidationLbl.Text = "*Enter valid user name";
             // 
             // pinValidationLbl
             // 
             this.pinValidationLbl.AutoSize = true;
             this.pinValidationLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pinValidationLbl.ForeColor = System.Drawing.Color.Red;
-            this.pinValidationLbl.Location = new System.Drawing.Point(257, 472);
+            this.pinValidationLbl.Location = new System.Drawing.Point(190, 472);
             this.pinValidationLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.pinValidationLbl.Name = "pinValidationLbl";
-            this.pinValidationLbl.Size = new System.Drawing.Size(120, 20);
+            this.pinValidationLbl.Size = new System.Drawing.Size(126, 20);
             this.pinValidationLbl.TabIndex = 5;
-            this.pinValidationLbl.Text = "Enter valid PIN";
+            this.pinValidationLbl.Text = "*Enter valid PIN";
             // 
-            // lblLockOutTimer
+            // labelLockoutTimer
             // 
-            this.lblLockOutTimer.AutoSize = true;
-            this.lblLockOutTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLockOutTimer.ForeColor = System.Drawing.Color.White;
-            this.lblLockOutTimer.Location = new System.Drawing.Point(503, 159);
-            this.lblLockOutTimer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblLockOutTimer.Name = "lblLockOutTimer";
-            this.lblLockOutTimer.Size = new System.Drawing.Size(53, 20);
-            this.lblLockOutTimer.TabIndex = 6;
-            this.lblLockOutTimer.Text = "label6";
+            this.labelLockoutTimer.AutoSize = true;
+            this.labelLockoutTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLockoutTimer.ForeColor = System.Drawing.Color.White;
+            this.labelLockoutTimer.Location = new System.Drawing.Point(431, 157);
+            this.labelLockoutTimer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLockoutTimer.Name = "labelLockoutTimer";
+            this.labelLockoutTimer.Size = new System.Drawing.Size(0, 20);
+            this.labelLockoutTimer.TabIndex = 6;
             // 
             // loginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
-            this.ClientSize = new System.Drawing.Size(1040, 690);
-            this.Controls.Add(this.lblLockOutTimer);
+            this.ClientSize = new System.Drawing.Size(846, 690);
+            this.Controls.Add(this.labelLockoutTimer);
             this.Controls.Add(this.pinValidationLbl);
             this.Controls.Add(this.usernameValidationLbl);
             this.Controls.Add(this.label5);
@@ -280,12 +280,14 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.guna2CirclePictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "loginForm";
             this.Padding = new System.Windows.Forms.Padding(40, 37, 40, 37);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "loginForm";
             this.Load += new System.EventHandler(this.loginForm_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouse_Down);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mouse_Move);
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -306,6 +308,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label usernameValidationLbl;
         private System.Windows.Forms.Label pinValidationLbl;
-        private System.Windows.Forms.Label lblLockOutTimer;
+        private System.Windows.Forms.Label labelLockoutTimer;
     }
 }
--- spend-smart/loginForm.cs
+++ spend-smart/loginForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,30 @@ namespace spend_smart
 {
     public partial class loginForm : Form
     {
+        public Point mouseLocation;
+        private int attempts = 0;
+        private DateTime lockoutEndTime;
+
         public loginForm()
         {
             InitializeComponent();
+        }
+
+        // Dragging part
+        private void mouse_Down(object sender, MouseEventArgs e)
+        {
+            mouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        // Dragging part
+        private void mouse_Move(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePose = Control.MousePosition;
+                mousePose.Offset(mouseLocation.X, mouseLocation.Y);
+                Location = mousePose;
+            }
         }
 
         private void loginForm_Load(object sender, EventArgs e)
@@ -23,13 +45,69 @@ namespace spend_smart
             pinValidationLbl.Visible = false;
         }
 
-        
-
         private void loginBtn_Click(object sender, EventArgs e)
         {
-            menuControls dashboard = new menuControls();
-            dashboard.Show();
-            this.Hide();
+            if (DateTime.Now < lockoutEndTime)
+            {
+                TimeSpan remainingTime = lockoutEndTime - DateTime.Now;
+                string remainingTimeString = $"{(int)remainingTime.TotalMinutes}:{remainingTime.Seconds:00}";
+                MessageBox.Show($"You are locked out. Please try again after {remainingTimeString}.");
+                return;
+            }
+
+            if (userNameTxt.Text == "Admin" && passwordTxtBox.Text == "123")
+            {
+                menuControls dashboard = new menuControls();
+                dashboard.Show();
+                this.Hide();
+                ResetAttempts(); // Reset attempts on successful login
+            }
+            else
+            {
+                attempts++;
+
+                if (attempts >= 3)
+                {
+                    lockoutEndTime = DateTime.Now.AddMinutes(1); // Lockout for 1 minute
+                    MessageBox.Show($"You have reached maximum number of attempts.\n Login will be disabled for 1 minute.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    MessageBox.Show($"Invalid credentials. Attempts done: {attempts} out of 3", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+
+            if (attempts >= 3)
+            {
+                loginBtn.Enabled = false; // Disable login button during lockout period
+                labelLockoutTimer.Visible = true; // Show lockout timer label
+                Timer timer = new Timer();
+                timer.Interval = 1000; // 1 second
+                timer.Tick += Timer_Tick;
+                timer.Start();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now >= lockoutEndTime)
+            {
+                ((Timer)sender).Stop();
+                loginBtn.Enabled = true; // Re-enable login button after lockout period
+                labelLockoutTimer.Visible = false; // Hide lockout timer label
+                ResetAttempts(); // Reset attempts after lockout period ends
+            }
+            else
+            {
+                TimeSpan remainingTime = lockoutEndTime - DateTime.Now;
+                string remainingTimeString = $"{(int)remainingTime.TotalMinutes}:{remainingTime.Seconds:00}";
+                labelLockoutTimer.Text = $"Lockout remaining: {remainingTimeString}";
+            }
+        }
+
+        private void ResetAttempts()
+        {
+            attempts = 0;
         }
 
         private void userNameTxt_Enter(object sender, EventArgs e)
