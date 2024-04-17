14983c5c98895cc04f89203b4dc5070f48e45a27
charukaonline <nuwanthakonline@gmail.com>
2024-04-17 20:28:52 +0530

    show expenses ui update with data grid

--- spend-smart/showExpenses.cs
+++ spend-smart/showExpenses.cs
@@ -16,25 +16,25 @@ namespace spend_smart
         {
             InitializeComponent();
 
-            ThemeManage.AddControlToColor(guna2Panel2);
-            ThemeManage.AddControlToColor(label1);
-            ThemeManage.AddControlToColor(label2);
-            ThemeManage.AddControlToColor(label3);
-            ThemeManage.AddControlToColor(label4);
-            ThemeManage.AddControlToColor(label6);
-            ThemeManage.AddControlToColor(label8);
-            ThemeManage.AddControlToColor(label9);
-            ThemeManage.AddControlToColor(label10);
-            ThemeManage.AddControlToColor(label12);
-            ThemeManage.AddControlToColor(label13);
-            ThemeManage.AddControlToColor(label14);
-            ThemeManage.AddControlToColor(label16);
-            ThemeManage.AddControlToColor(label17);
-            ThemeManage.AddControlToColor(label18);
-            ThemeManage.AddControlToColor(label20);
-            ThemeManage.AddControlToColor(label21);
+            //ThemeManage.AddControlToColor(guna2Panel2);
+            //ThemeManage.AddControlToColor(label1);
+            //ThemeManage.AddControlToColor(label2);
+            //ThemeManage.AddControlToColor(label3);
+            //ThemeManage.AddControlToColor(label4);
+            //ThemeManage.AddControlToColor(label6);
+            //ThemeManage.AddControlToColor(label8);
+            //ThemeManage.AddControlToColor(label9);
+            //ThemeManage.AddControlToColor(label10);
+            //ThemeManage.AddControlToColor(label12);
+            //ThemeManage.AddControlToColor(label13);
+            //ThemeManage.AddControlToColor(label14);
+            //ThemeManage.AddControlToColor(label16);
+            //ThemeManage.AddControlToColor(label17);
+            //ThemeManage.AddControlToColor(label18);
+            //ThemeManage.AddControlToColor(label20);
+            //ThemeManage.AddControlToColor(label21);
 
-            this.Load += showExpenses_Load;
+            //this.Load += showExpenses_Load;
         }
 
         private void showExpenses_Load(object sender, EventArgs e)
